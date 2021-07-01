35e332d44b266bf2cbc8ea3839b5c03b2bf21e8f
kage1011 <khaivv123@gmail.com>
2021-07-01 21:05:40 +0700

    make it better

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Chị iu Vương Thuý Nga của tui à!',
+    introDesc: `chào mừng chị tới với cái website nhảm nhí này`,
+    btnIntro: 'nhấn zô đây nè :>',
+    title: 'gửi đến một người bạn của tui 😙',
+    desc: 'Tuổi mới, tôi hi vọng bố mẹ bà sẽ sớm được "tống cổ" bà ra khỏi nhà nhé! Bà than ế hơi lâu rồi đấy!' ,
+    desc2: 'Thanh nói "Tất cả những người được sinh ra vào ngày hôm nay đều vô cùng tuyệt vời.Tất nhiên cậu là một NGOẠI LỆ". Chúc yang hồ mõm của bọn tui bớt chém gió lại,ít than vãn,bớt khẩu nghiệp đi nhé !',
+    desc3: '2 chị em tui dạo này nghèo lắm ,cho xin tí đầu tư đi khởi nghiệp nhé :3 !',
+    btnYes: 'OK chuyện nhỏ :v',
+    btnNo: 'Không, Hết tiền rồi !!',
+    question:'Nói thế thôi! bạn tui mong muốn tuổi mới sẽ như thế nào nè <3, chỉ mình bà biết thôi !!! ',
+    btnReply: 'send đê :v',
+    reply: '',
+    mess: 'tụi tui không đọc được đâu,nhưng sẽ luôn luôn ủng hộ bà :3',
+    messDesc: 'Hãy luôn cười,luôn luôn hạnh phúc nhé,chúc bạn của tui có được mọi thứ mà cậu mong muốn ',
+    btnAccept: 'End',
+    messLink: 'https://www.facebook.com/messages/t/2766860160061114' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
